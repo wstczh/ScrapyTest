b5176c5b5abf178b5ea90d977244fd44d961a6ad
wstczh <zhuhao9876@qq.com>
2018-06-05 13:32:29 +0800

    Add files via upload

---
+++ city58/city58/items.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class City58Item(scrapy.Item):
+    # define the fields for your item here like:
+    name = scrapy.Field()
+    price=scrapy.Field()
+    url=scrapy.Field()
+    introduce_item=scrapy.Field()
+    address=scrapy.Field()
+    phone_number=scrapy.Field()
---
+++ city58/city58/main.py
@@ -0,0 +1,2 @@
+from scrapy.cmdline import execute
+execute('scrapy crawl city58_test'.split())
